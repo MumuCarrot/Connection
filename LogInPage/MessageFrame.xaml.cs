7660da9209b24f1d78839b7fe93ebf7b30730b8f
Иван Коваленко <ivan.kovalenko8080@gmail.com>
2024-04-01 14:42:35 +0200

    Message Frame update

---
+++ LogInPage/MessageFrame.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace LogInPage
+{
+    public partial class MessageFrame : UserControl
+    {
+        public MessageFrame(Message message, bool? isMy)
+        {
+            InitializeComponent();
+
+            username.Text = message.Login;
+            content.Text = message.Content;
+            time.Text = DateTime.Parse(message.MessageDateTime).ToString("HH:mm");
+
+            if (isMy is not null && (bool)isMy)
+            {
+                frame.HorizontalAlignment = HorizontalAlignment.Right;
+                frame.CornerRadius = new(8, 8, 0, 8);
+            }
+            else 
+            {
+                frame.HorizontalAlignment = HorizontalAlignment.Left;
+                frame.CornerRadius = new(8, 8, 8, 0);
+            }
+        }
+    }
+}
